2bf32c640674aa66ecf2ab9d5271b3a48a74d35a
Hammad <hmdshfq@outlook.com>
2021-12-19 17:34:16 +0100

    add geolocation api

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import SeasonDisplay from './components/SeasonDisplay'
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  )
   return <div>Hi there</div>
 }
 
