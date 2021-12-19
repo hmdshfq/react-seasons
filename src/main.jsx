4c4ab3eba4aa5a8e95d9a2c488b93e63c26461ed
Hammad <hmdshfq@outlook.com>
2021-12-19 17:57:01 +0100

    update comment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import SeasonDisplay from './components/SeasonDisplay'
 
 class App extends React.Component{
   render() {
-    // getting geolocation takes time, it is an async operation
+    /**
+     * Getting geolocation takes time, it is an async operation therefore 
+     * it requires the component to be rerendered to show the updated information.
+     * For rerendering the component we have to use state.
+     */
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
       (error) => console.log(error)
