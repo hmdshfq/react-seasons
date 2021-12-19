3d5f5c1cd22b79540b91fc4d22407d0b9583e90d
Hammad <hmdshfq@outlook.com>
2021-12-19 17:48:13 +0100

    add comment

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import SeasonDisplay from './components/SeasonDisplay'
 
 class App extends React.Component{
   render() {
+    // getting geolocation takes time, it is an async operation
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
       (error) => console.log(error)
