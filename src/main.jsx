7cb1217e277a63171cc00c2a7a0f818c4e3c118a
Hammad <hmdshfq@outlook.com>
2021-12-19 17:45:46 +0100

    replace App component to class

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './components/SeasonDisplay'
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.log(error)
-  )
-  return <div>Hi there</div>
+class App extends React.Component{
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.log(error)
+    )
+    return <div>Latitude: </div>
+  }
 }
 
 ReactDOM.render(
