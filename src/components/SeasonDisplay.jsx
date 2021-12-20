84b9cc5a507fac963ef309f859c28d7affb2dc59
Hammad <hmdshfq@outlook.com>
2021-12-20 19:25:26 +0100

    add more logic & icons

--- src/components/SeasonDisplay.jsx
+++ src/components/SeasonDisplay.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const getSeason = (lat, month) => {
     /**
      * Supposition
-     * 
+     *
      * Northern Hemisphere
      * - Winter is from October until March
      * - Summer is from April until September
@@ -12,16 +12,25 @@ const getSeason = (lat, month) => {
      * - Winter is from April until September
      */
     if (month > 2 && month < 9) {
-        return lat > 0 ? 'Summer' : 'Winter'
+        return lat > 0 ? 'summer' : 'winter';
     } else {
-        return lat > 0 ? 'Winter' : 'Summer'
+        return lat > 0 ? 'winter' : 'summer';
     }
-}
+};
 
 const SeasonDisplay = props => {
     // props.lat gives the latitute and getMonth will give month to the function
-    const season = getSeason(props.lat, new Date().getMonth())
-    return <h1>{ season }</h1>
-}
+    const season = getSeason(props.lat, new Date().getMonth());
+    const message =
+        season === 'winter' ? `Burr, it's chilly!` : `Let's hit the beach!`;
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
+    return (
+        <div>
+            <i className={ `${icon} icon` }/>
+            <h1>{message}</h1>
+            <i className={ `${icon} icon` }/>
+        </div>
+    );
+};
 
-export default SeasonDisplay
+export default SeasonDisplay;
